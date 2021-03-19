86de7ce145fdd32d422f79f71a1749fc9864f829
Gleb <gleb.mirzazyanov@gmail.com>
2021-03-19 23:56:17 +0300

    Deploy each module added

--- bot/src/main/java/com/intern/bot/HelloWorldController.java
+++ bot/src/main/java/com/intern/bot/HelloWorldController.java
@@ -1,16 +1,15 @@
-package com.intern.api;
+package com.intern.bot;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @Controller
 public class HelloWorldController {
 
-    private static final String template = "Hello from api service, %s!";
+    private static final String template = "Hello from bot service, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/hello-world")
